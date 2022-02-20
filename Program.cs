c9e470fa8636353a74f46d1e39e51a3cd4d85e95
Starkash <99684327+Starkash@users.noreply.github.com>
2022-02-20 19:48:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Pallendrum
+{
+    class Pallendrum
+    {
+        static void Main()
+        {
+            int num, result = 0, rem, originalnum;
+
+            Console.WriteLine("Eneter Any Number:");
+            num = int.Parse(Console.ReadLine());
+            originalnum = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                result = rem + result * 10;
+                num = num / 10;
+
+
+            }
+
+            if (result == originalnum)
+            {
+                Console.WriteLine("This is a Pallendrum Number:");
+
+            }
+            else
+            {
+                Console.WriteLine("This is not Pallendrum Number:");
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
